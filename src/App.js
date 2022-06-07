acba58134f9f83d36c07c300ec62dc723fdbbd81
kangurk <kiur.kangur1@gmail.com>
2022-06-07 20:14:01 +0300

    Added hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header.tsx'
+import {Hero} from './components/Hero/Hero.tsx'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import "./Hero.scss"
+
+export const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="heroText">
+                <h1>TAAVI POST</h1>
+                <h1>FIGMA KONSULTATSIOONID</h1>
+                <h1>10+ AASTAT KOGEMUST</h1>
+            </div>
+        </div>
+    )
+}
